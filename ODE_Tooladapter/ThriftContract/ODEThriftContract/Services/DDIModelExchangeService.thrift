9571f15cfca3b11df3fa8740ff7717a476c590ca
Joshua Frey <josh.frey89@gmail.com>
2019-03-15 16:13:04 +0100

    Removed old Thrift Contract

--- ODE_Tooladapter/ThriftContract/ODEThriftContract/Services/DDIModelExchangeService.thrift
+++
@@ -1,6 +0,0 @@
-
-service DDIModelExchangeService{
-    void ExportModelToDDIFile(1: string Path, 2: TDDIAbstractODEPackage ODEPackage) throws (1: TDDIValidationFailedException ValidationFailedException),
-    TDDIAbstractODEPackage ImportDDIModel(1: string Path) throws (1: TDDIValidationFailedException ValidationFailedException),
-    TDDIValidationResult ValidateDdi(1: string DdiPath, 2: string EvlFilePath)
-}
