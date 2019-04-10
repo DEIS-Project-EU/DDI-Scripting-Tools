016ec0ef2ed84e40b450158deae9cd4b55cd0656
Ioannis Sorokos <yanos2soros@gmail.com>
2019-04-10 18:36:18 +0100

    Update apiDDIBuilderHelpers.js

--- Documentation/apiDDIBuilderHelpers.js
+++ Documentation/apiDDIBuilderHelpers.js
@@ -1,16 +1,33 @@
-apijson = {
+apijson2 = {
   "Clone": {
-	"context": "Any",
-	"return-type": "Any",
+	"context": {
+	  "type": "Any",
+	  "!text": "The subject EMF element to be cloned"
+	},
+	"return-type": {
+	  "type": "Any",
+	  "!text": "The clone of the context EMF element"
+	},
 	"comments": {
-	  "!text": "Clone deep-copies a EMF element using org.eclipse.epsilon.emc.emf.tools.EmfTool.EcoreUtil.copy.\nFor the cloned element to be usable (properties accessible), it must be copied into a container element in the subject model"
+	  "!text": "Clone deep-copies a EMF element using org.eclipse.epsilon.emc.emf.tools.EmfTool.EcoreUtil.copy \n<br><b>IMPORTANT: </b>For the cloned element to be usable (properties accessible), it must be copied into a container element in the subject model"
 	}
   },
   "SetNameDescription": {
-	"context": "BaseElement",
+	"context": {
+	  "type": "BaseElement",
+	  "!text": "The BaseElement whose name and description should be set"
+	},
 	"param": [
-	  "name : String",
-	  "description : String"
+	  {
+		"!value": "name",
+		"type": "String",
+		"!text": "The name the element should be set to"
+	  },
+	  {
+		"!value": "description",
+		"type": "String",
+		"!text": "The description the element should be set to"
+	  }
 	],
 	"comments": {
 	  "!text": "SetNameDescription sets a BaseElement's name and description"
